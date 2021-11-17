1e88d2dc66f01f38de9b2ba2177bc495639c8dd3
cherif diallo <riftone07@gmail.com>
2021-11-17 22:00:26 +0000

    creation migration et model

---
+++ database/migrations/2021_11_17_205701_create_produits_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProduitsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('produits', function (Blueprint $table) {
+            $table->id();
+            $table->string('nom');
+            $table->longText('description')->nullable();
+            $table->integer('prix');
+            $table->integer('quantite')->default(0);
+            $table->boolean('active')->default(true);
+            $table->string('image');
+            $table->foreignId('categorie_id')->constrained('categories');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('produits');
+    }
+}
---
+++ database/migrations/2021_11_17_210334_create_commandes_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCommandesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('commandes', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('reference')->unique();
+
+            $table->integer('total');
+
+            $table->foreignId('user_id')->constrained('users');
+
+            $table->string('status')->default('commande reçue');
+
+            $table->string('adresse_de_livraison');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('commandes');
+    }
+}
