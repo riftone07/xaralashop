08899302bcd7562ca2ed8cbf11d947fd3f42efd6
cherif diallo <riftone07@gmail.com>
2022-02-20 21:27:44 +0000

    Ajout d'un log

--- app/Http/Controllers/API/TraitementController.php
+++ app/Http/Controllers/API/TraitementController.php
@@ -5,11 +5,16 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use App\Models\Commande;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class TraitementController extends Controller
 {
     public function callback()
     {
+        Log::info("-----------------callback reussi------------------");
+        Log::info($_POST['data']);
+        Log::info("-----------------callback reussi------------------");
+
         try {
             //Prenez votre MasterKey, hashez la et comparez le résultat au hash reçu par IPN
             if($_POST['data']['hash'] === hash('sha512', env("P_MasterKey"))) {
